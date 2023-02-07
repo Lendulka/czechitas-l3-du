88e1a1dc3c12d1b88cc782faff890331cb59ae13
Lenka Černohorská <cernohorska.lc@gmail.com>
2023-02-07 21:21:14 +0100

    HOTOVO

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 
 let jmeno = prompt("Jaké je Tvoje jméno?");
 let prijmeni = prompt("Jaké je Tvoje příjmení?");
-let rokNaroz = Number(prompt("Napiš rok narození"));
+let rokNaroz = Number(prompt("Napiš rok narození", "RRRR"));
 
 const aktualRok = 2023;
 
@@ -12,7 +12,7 @@ function spoctiVek() {
     vystupProgramu.innerHTML = jmeno + " " + prijmeni + " " + vek;
 }
 
-let barva = prompt("Jaká je Tvoje oblíbená barva?");
+let barva = prompt("Jaká je Tvoje oblíbená barva?", "in English");
 
 function zmenBarvu() {
     let vystup = document.querySelector(".vystup");
